dc3649f72e6f76e95e2ccd2042ee4e8cc88ccd0d
CaioRS <caiorss.rodrigues@gmail.com>
2015-02-14 10:07:19 -0300

    functional.js updated

--- functional.js
+++ functional.js
@@ -298,29 +298,6 @@ function object_keys(obj){
 
 
 
-//linspace.js Generate linearly spaced vectors
-//Helena F. Deus (helenadeus@gmail.com)
-//200809
-//
-//  > linspace(0, 8, 4)
-//  [0, 2.6667, 5.3334, 8]
-//
-function linspace(d1,d2,n) {
-               
-        j=0;
-        var L = new Array();
-       
-        while (j<=(n-1)) {
-       
-                var tmp1 = j*(d2-d1)/(Math.floor(n)-1);
-                var tmp2 = Math.ceil((d1+tmp1)*10000)/10000;
-                L.push(tmp2);
-                j=j+1;
-        }
-       
-        return L;
- }
-
 
 //  Get the maximum number in a array
 //
@@ -897,6 +874,14 @@ function Selector(List){
                 
     this.remove = function(){
             this.List.map(function(e){e.remove() }) ;
+    };
+    
+    this.children = function(){
+    
+        return new Selector(this.List[0].children)
+    };
+    
+    
     };
     
 }; // End Of Selector
